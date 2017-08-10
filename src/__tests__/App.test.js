54a684da99a7a20eea2ff486a2e65cec1d7ffa0d
Mario Souto <soutomarios@gmail.com>
2017-08-10 01:01:13 -0300

    Fix package.json npm run test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,10 +2,7 @@ import request from 'supertest'
 import App from '../server/App'
 
 describe('Test the root path', () => {
-  test('It should response the GET method', (done) => {
-    request(App).get('/').then((response) => {
-      expect(response.statusCode).toBe(200)
-      done()
-    })
+  test('It should response the GET method', () => {
+      return request(App).get('/').expect(200);
   })
 })
