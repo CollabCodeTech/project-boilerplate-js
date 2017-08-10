c0759ceb536794bd16e95989fe7ca66c1dab3d16
Mario Souto <soutomarios@gmail.com>
2017-08-10 00:12:06 -0300

    Additing support for webpack at server and Standard linter

--- readme.md
+++ readme.md
@@ -2,7 +2,9 @@
 
 [![licence mit](https://img.shields.io/badge/licence-MIT-blue.svg)](https://github.com/CollabCodeBR/collabcodebr-project-boilerplate/blob/master/LICENSE.md)
 
-# Example
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+#
 
 Lorem...
 
---
+++ webpack.config.babel.js
@@ -0,0 +1,49 @@
+import { join } from 'path'
+import webpack from 'webpack'
+import nodeExternals from 'webpack-node-externals'
+
+const paths = {
+  root: join(__dirname, '..'),
+  publicSrc: join(__dirname, 'src', 'app'),
+  publicDist: join(__dirname, 'src', 'dist'),
+  serverSrc: join(__dirname, 'src', 'server'),
+  serverDist: join(__dirname, 'src', 'server', 'dist')
+}
+
+const server = {
+  target: 'node',
+  node: {
+    __dirname: false
+  },
+  externals: [nodeExternals({
+    modulesFromFile: true
+  })],
+  entry: {
+    js: join(paths.serverSrc, 'server.js')
+  },
+  output: {
+    path: paths.serverDist,
+    filename: 'www',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      { // Standard Linter
+        enforce: 'pre',
+        test: /\.js$/,
+        include: paths.serverSrc,
+        exclude: [/node_modules/],
+        use: 'standard-loader'
+      },
+      { // Babel Transpile ESLatest for ES5
+        test: join(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+          options: 'cacheDirectory=.babel_cache'
+        }
+      }
+    ]
+  }
+}
+
+export default server
